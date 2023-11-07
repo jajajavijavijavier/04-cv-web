6ab83814165bac5934a65a29fda5e8723bf2ea77
javier <jagaraypa@ucq.edu.mx>
2023-11-06 22:14:45 -0600

    puntuacion

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import './header.css';
-import './lettersName.css';
-import './aboutMe.css';
+import './header.css'
+import './lettersName.css'
+import './aboutMe.css'
 
-const rootElement = document.getElementById('root');
+const rootElement = document.getElementById('root')
 ReactDOM.createRoot(rootElement).render(
   <React.StrictMode>
     <App />
